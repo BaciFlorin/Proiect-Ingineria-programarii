e86876413872e8befcc2d429eef810382936fc42
virgilciulei23 <61265474+virgilciulei23@users.noreply.github.com>
2020-05-11 20:59:49 +0300

    Add files via upload

---
+++ DLLentitati/DLLentitati/DLLentitati/Cerere.cs
@@ -0,0 +1,43 @@
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        Cerere.cs                                                *
+ *  Copyright:   (c) 2020, Ciulei Virgil                                  *
+ *  E-mail:      virgil.ciulei@student.tuiasi.ro                          *
+ *  Description: Clasa care stocheaza informatiile despre o cerere din    *
+ *               baza de date.                                            *
+ *                                                                        *
+ **************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entitati
+{
+    public class Cerere
+    {
+        /// <summary>
+        /// Constructor clasa
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="id_carte"></param>
+        /// <param name="id_user"></param>
+        /// <param name="tip_cerere"></param>
+        public Cerere(int id, int id_carte, int id_user, string tip_cerere)
+        {
+            ID = id;
+            ID_Carte = id_carte;
+            ID_User = id_user;
+            Tip_cerere = tip_cerere;
+        }
+
+        public int ID { get; private set; }
+
+        public int ID_Carte { get; private set; }
+
+        public int ID_User { get; private set; }
+
+        public string Tip_cerere { get; private set; }
+
+    }
+}
---
+++ DLLentitati/DLLentitati/DLLentitati/User.cs
@@ -0,0 +1,38 @@
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        User.cs                                                  *
+ *  Copyright:   (c) 2020, Ciulei Virgil                                  *
+ *  E-mail:      virgil.ciulei@student.tuiasi.ro                          *
+ *  Description: Clasa care stocheaza informatiile despre un user din     *
+ *               baza de date.                                            *
+ *                                                                        *
+ **************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entitati
+{
+    public class User
+    {
+        /// <summary>
+        /// Constructor clasa
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user_name"></param>
+        /// <param name="parola"></param>
+        public User(int id, string user_name, string parola)
+        {
+            ID = id;
+            User_name = user_name;
+            Parola = parola;
+        }
+
+        public int ID { get; private set; }
+
+        public string User_name { get; private set; }
+
+        public string Parola { get; private set; }
+    }
+}
