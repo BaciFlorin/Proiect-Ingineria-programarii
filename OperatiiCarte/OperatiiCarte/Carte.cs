3d9b7a22dd016d084e34b530a6f59e6984d283e0
Bacica Florin <bacicaflorin@gmail.com>
2020-05-11 16:25:21 +0300

    Adaugare comentarii peste cod

--- OperatiiCarte/OperatiiCarte/Carte.cs
+++ OperatiiCarte/OperatiiCarte/Carte.cs
@@ -1,37 +1,94 @@
-﻿using System;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        Carte.cs                                                 *
+ *  Copyright:   (c) 2020, Bacica Florin                                  *
+ *  E-mail:      florin.bacica@student.tuiasi.ro                          *
+ *  Description: Clasa care stocheaza informatiile despre o carte din     *
+ *               baza de date.                                            *
+ *                                                                        *
+ **************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SqliteHandle
+namespace Entitati
 {
-    class Carte
+    public class Carte
     {
-        public Carte(int id, string titlu, string autor, string ibbn, string categorie, string descriere, int stoc)
+        /// <summary>
+        /// Constructor clasa
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="titlu"></param>
+        /// <param name="autor"></param>
+        /// <param name="ibsn"></param>
+        /// <param name="categorie"></param>
+        /// <param name="descriere"></param>
+        /// <param name="stoc"></param>
+
+        public Carte(int id, string titlu, string autor, string ibsn, string categorie, string descriere, int stoc)
         {
-            ID = id;
+            Id = id;
             Autor = autor;
             Titlu = titlu;
-            IBBN = ibbn;
+            Ibsn = ibsn;
             Categorie = categorie;
             Descriere = descriere;
             Stoc = stoc;
         }
 
-        public int ID { get; private set; }
+        /// <summary>
+        /// Id-ul unic atribuit fiecarei carti in baza de date
+        /// </summary>
+        public int Id { get; set; }
 
-        public string Titlu { get; private set; }
+        /// <summary>
+        /// Titlul cartii
+        /// </summary>
+        public string Titlu { get; set; }
 
-        public string Autor { get; private set; }
+        /// <summary>
+        /// Autorul cartii
+        /// </summary>
+        public string Autor { get; set; }
 
-        public string Categorie { get; private set; }
+        /// <summary>
+        /// Gen-ul din care face parte cartea
+        /// </summary>
+        public string Categorie { get; set; }
 
-        public string Descriere { get; private set; }
+        /// <summary>
+        /// Descrierea sumara a cartii
+        /// </summary>
+        public string Descriere { get; set; }
 
-        public string IBBN { get; private set; }
+        /// <summary>
+        /// Cod internaţional de identificare a cărţilor
+        /// </summary>
+        public string Ibsn { get; set; }
 
-        public int Stoc { get; private set; }
+        /// <summary>
+        /// Stocul cartii
+        /// </summary>
+        public int Stoc { get; set; }
 
+        /// <summary>
+        /// Metoda folosita pentru reprezentarea in format text a cartii
+        /// </summary>
+        public override string ToString()
+        {
+            string carte = "";
+            carte += "Titlu:" + this.Titlu + "\n";
+            carte += "Autor:" + this.Autor + "\n";
+            carte += "Categorie:" + this.Categorie + "\n";
+            carte += "Descriere:" + this.Descriere + "\n";
+            carte += "IBSN:" + this.Ibsn + "\n";
+            carte += "Stoc:" + this.Stoc + "\n";
+
+            return carte;
+        }
     }
 }
